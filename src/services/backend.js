8d2d5967a4d3295653b79b97b35344c2b8575254
Jolan PLOT <j.plot@sstrn.com>
2021-12-09 14:25:07 +0100

    appel api OK

--- src/services/backend.js
+++ src/services/backend.js
@@ -1,23 +1,38 @@
 import Axios from "axios";
 
 const apiClient = Axios.create({
-    baseURL: `${process.env.API_URL}`,
-    timeout: 3000,
+  baseURL: `${process.env.VUE_APP_API_URL}`,
+  timeout: 3000,
+  headers: {
+    Authorization:
+      "Cryptum-Token 6WMbEb98FNACgUlJz2XdxhD9fuox8GT2ewuRMqu1hOz1lBi0DOKSACULEHwg7gn4",
+    "Content-Type": "application/json",
+    "Cryptum-API-Version": "2.3-alpha",
+  },
 });
 
 export default {
-    instance: apiClient,
-    url : {
-        haloInfinite : '/hi/',
-        haloHMCC: '/hmcc/',
-        haloFive : '/h5/'
-    },
-    
-    getHIUser(gamerTag) {
-        return this.instance
-            .get(`${url.haloInfinite}stats/players/${gamerTag}/csrs?season=1`)
-            .then((response) => {
-                    console.log('response.data :>> ', response.data);
-        });
-    },
-}
+  instance: apiClient,
+  url: {
+    haloInfinite: "/hi/",
+    haloHMCC: "/hmcc/",
+    haloFive: "/h5/",
+  },
+
+  getHIUser(gamerTag) {
+    return this.instance
+      .get(`${this.url.haloInfinite}stats/players/${gamerTag}/csrs?season=1`)
+      .then((response) => {
+        console.log("response.data :>> ", response.data);
+      });
+  },
+  getRecentMatches(gamerTag) {
+    return this.instance
+      .get(
+        `${this.url.haloHMCC}stats/players/${gamerTag}/recent-matches?page=1`
+      )
+      .then((recentMatches) => {
+        return recentMatches.data;
+      });
+  },
+};
