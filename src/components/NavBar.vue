64428186082eb84ed87726ae810f98a1b5972f82
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2021-12-09 14:41:57 +0100

    fix: conflicts

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -4,6 +4,7 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/hmcc">HMCC</router-link>
+      <router-link :to="{ name: 'HaloInfinite' }">Halo Infinite</router-link>
     </div>
     <router-view />
   </div>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import HMCC from "../views/HMCC/Hmcc.vue"
+import HaloInfinite from "../views/HaloInfinite.vue";
 
 Vue.use(VueRouter);
 
@@ -24,6 +25,11 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/halo-infinite",
+    name: "HaloInfinite",
+    component: HaloInfinite,
   },
 ];
 
